075ef4f06ebdec12ccfc66087671e99cdbddbb54
Michael Patten <4432259+mcpatten@users.noreply.github.com>
2023-05-10 14:09:29 -0400

    test with explicit postcss include via `@storybook/addon-styling`

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,19 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import postcss from "postcss";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    {
+      name: "@storybook/addon-styling",
+      options: {
+        postCss: {
+          implementation: postcss,
+        },
+      },
+    },
   ],
   framework: {
     name: "@storybook/nextjs",
